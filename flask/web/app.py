c89d4719ac0ac894941765323eaaedb00b3928e8
kimsoyeong <iloveit356@gmail.com>
2021-01-27 14:52:32 +0900

    Update

--- flask/web/app.py
+++ flask/web/app.py
@@ -1,14 +1,13 @@
-import time
-# import pymysql
-
-from flask import Flask, request, jsonify, send_file
-import tensorflow as tf
 import tensorflow_hub as hub
+from flask import Flask, request, send_file, jsonify
+import tensorflow as tf
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import io
+import json
 
-# Load model
+##########모델 로드
+
 base_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
 
 placeholder = tf.keras.layers.Input(shape=(None, None, 3), dtype=tf.float32)
@@ -16,13 +15,13 @@ placeholder_1 = tf.keras.layers.Input(shape=(None, None, 3), dtype=tf.float32)
 net = hub.KerasLayer(base_model, signature='serving_default', signature_outputs_as_dict=True)({'placeholder': placeholder, 'placeholder_1': placeholder_1})
 model = tf.keras.models.Model({'placeholder': placeholder, 'placeholder_1': placeholder_1}, net)
 
+##########모델 예측
 
-# predict
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>HELLO</h1>'
+def index():
+    return "Hello World"    
 
 @app.route('/send_image', methods=['POST', 'GET'])
 def send_image():
@@ -55,14 +54,61 @@ def send_image():
             bytesIO = io.BytesIO()
             image.save(bytesIO, 'JPEG', quality=70)
             bytesIO.seek(0)
-            image.show()
+            # image.show() # [DEBUG]
             
             return send_file(bytesIO, mimetype='image/jpeg')
-            # return jsonify({"result": "test mode"})
         else:
-            return jsonify({"result": "failed to get image"})
+            return jsonify({"result": "error - failed to get the image"})
     else:
-        return "not post mode"
+        return "hello world"
+
+@app.route('/save_image', methods=['POST', 'GET'])
+def save_image():
+    if request.method == "POST":
+        success = False
+        image_data = request.get_json()
+        author = image_data['author']
+        name = image_data['name']
+        src = image_data['url']
+        date = datetime()
+
+        sql = f"INSERT INTO trans_images (author, name, src, date) VALUES('{author}', '{name}', '{src}', '{date}');"
+        success = conn_db(sql, "insert")
+        return jsonify({"success": success})
+    else:
+        return "hello world"
+
+@app.route('/get_album', methods=['POST', 'GET'])
+def get_album():
+    if request.method == "GET": 
+        sql = "SELECT id, author, name, src FROM trans_images;"
+        data = conn_db(sql, "select")
+        return json.dumps(data)
+    else:
+        return "hello world"
+
+
+def conn_db(sql, type):
+    import pymysql ## pip3 install pymysql 
+
+    conn = pymysql.connect(host='54.180.145.225', user='sv', password='Fighting0!', db='lambda') # git에 업로드할때는 꼭 지워주세요!!!
+    curs = conn.cursor(pymysql.cursors.DictCursor)
+    curs.execute(sql)
+    if type == "insert":
+        conn.commit()
+        conn.close()
+        return True
+    elif type == "select":
+        rows = curs.fetchall()
+        conn.close()
+        return rows
+
+    return False
+
+def datetime():
+    import datetime
+    now = datetime.datetime.now()
+    return now.strftime('%Y%m%d%H%M%S')
 
 
 if __name__ == '__main__':
